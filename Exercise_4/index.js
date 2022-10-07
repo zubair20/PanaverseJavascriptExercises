e7a161b55a4c1cb6338952e7b01d711f7ed39658
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 14:51:13 +0500

    Exercise 4 completed

---
+++ Exercise_4/index.js
@@ -0,0 +1,5 @@
+//Exercise 4: Program to display the famous quote
+
+let quote = "The future belongs to those who believe in the beauty of their dreams.";
+let authorName = "Eleanor Roosevelt"
+console.log(`${authorName} once said, ${quote}`)
