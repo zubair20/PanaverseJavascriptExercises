9322f1f0793a7b3391218785d916547cd9dd6d8c
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 14:54:34 +0500

    Exercise 5 completed

---
+++ Exercise_5/index.js
@@ -0,0 +1,6 @@
+//Exercise 5: Program to display the famous quote
+
+let quote = "The future belongs to those who believe in the beauty of their dreams.";
+let famous_person = "Eleanor Roosevelt"
+let message = `${famous_person} once said, ${quote}`
+console.log(message)
