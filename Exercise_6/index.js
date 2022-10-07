37b68a995507dfd62f57a9d17a8e35ebdf54920d
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 15:03:24 +0500

    Exercise 6 completed

---
+++ Exercise_6/index.js
@@ -0,0 +1,5 @@
+//Exercise 6: Program to print the name of the person after stripping white spaces
+
+let personName = "  \t\t\t   Zubair Akhtar \t\t\t \n   "
+console.log(personName)
+console.log(personName.trim())
