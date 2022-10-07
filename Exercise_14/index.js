915375fd6ad5f2df351081acc9e5403ab5beadb9
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 15:36:00 +0500

    Exercise 14 is completed

---
+++ Exercise_14/index.js
@@ -0,0 +1,4 @@
+//Exercise 14: Program to print names of guests and invite them to dinner
+
+let guests = ["Mohsin", "Haseeb", "Zubair", "Uzair", "Umair"]
+guests.forEach(guest => console.log(`I would like to invite ${guest} for the dinner`))
