bbc309e68ddd8b9f212104545d95295b0bb41411
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 15:11:00 +0500

    Exercise 9 completed

--- Exercise_7_8/index.js
+++ Exercise_7_8/index.js
@@ -1,3 +1,5 @@
+//Exercise 7 and 8: Program to print 8 on each line
+
 console.log(16 - 8)
 console.log(4 + 4)
 console.log(64 / 8)
---
+++ Exercise_9/index.js
@@ -0,0 +1,5 @@
+//Exercise 9: Program to print favorite number
+
+let favoriteNumber = 3;
+let message = `My Favorite Number is ${favoriteNumber}`
+console.log(message)
