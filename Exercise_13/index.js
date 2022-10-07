1ec06953dcec2c70ac5575441f1f16d3ec88de38
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 15:31:41 +0500

    Exercise 13 is completed

---
+++ Exercise_13/index.js
@@ -0,0 +1,4 @@
+//Exercise 13: Program to print your Favrite items
+
+let favItems = ["Civic Car", "Toyota Parado", "Range Rover", "Honda 125", "Aeroplane"]
+favItems.forEach(item => console.log(`I would like to own a ${item}`))
