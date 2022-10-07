2dbffa0923b194d00d30ea1ccc9ebfe4f5561b46
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 14:34:43 +0500

    Exercise 2 completed

--- Exercise_2/index.js
+++ Exercise_2/index.js
@@ -1,1 +1,4 @@
-console.log('Hellow')
+//Exercise 2: Program to display the name of the person stored in a variable
+
+let personName = "Zubair Akhtar";
+console.log(`Hello ${personName}, would you like to learn some Python today?`)
