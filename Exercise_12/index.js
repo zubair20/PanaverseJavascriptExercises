02cbe2af118aaed54a3db419fbc525aec2d85903
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 15:26:49 +0500

    Exercise 12 is completed

---
+++ Exercise_12/index.js
@@ -0,0 +1,4 @@
+//Exercise 12: Program to print personalized message to your friends
+
+let names = ["Mohsin", "Haseeb", "Zubair", "Uzair", "Umair"]
+names.forEach(name => console.log(`${name} is my best friend`))
