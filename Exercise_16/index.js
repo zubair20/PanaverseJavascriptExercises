338df91630f2375fb5ce6add5e5b04dbe5ab4ebe
zubair20 <zubair.akhtar20@gmail.com>
2022-10-07 15:51:57 +0500

    Exercise 16 is completed

---
+++ Exercise_16/index.js
@@ -0,0 +1,9 @@
+//Exercise 16: Program to print names of guests and invite them to dinner for bigger table
+let newGuest1 = "Waseem"
+let newGuest2 = "Anees"
+let newGuest3 = "Zahid"
+let guests = ["Mohsin", "Haseeb", "Zubair", "Uzair", "Umair"]
+guests.unshift(newGuest1)
+guests.splice(guests.length / 2, 0, newGuest2)
+guests.push(newGuest3)
+guests.forEach(guest => console.log(`I would like to invite ${guest} for the dinner`))
